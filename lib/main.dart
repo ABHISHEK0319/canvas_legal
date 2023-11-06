a0ccadc0698670c74d769f2dfe267a2800ed9460
Abhishek Maurya <abhishekmaurya1219@gmail.com>
2023-11-06 11:34:56 +0530

    commit3

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,7 @@ import 'package:canvas_legal/pages/canvas_legal.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: "AIzaSyACv4eCyGagEsiIwGc1uR9ZsxHGpfscNzo",
-      projectId: "canvas-legel",
-      storageBucket: "canvas-legel.appspot.com",
-      messagingSenderId: "66775404066",
-      appId: "1:66775404066:web:32473dd451e8aaa1ae447b",
-    ),
-  );
+void main() {
   runApp(const MyApp());
 }
 
